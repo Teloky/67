2b1b7873e3749f62eee2feae7b092dfeb92e8e11
Lilly <108250586+Teloky@users.noreply.github.com>
2025-08-13 23:50:14 -0500

    Update 67.js

--- 67.js
+++ 67.js
@@ -1,7 +1,7 @@
 let number = Math.floor(Math.random() * 100);
 document.getElementById("rng").textContent = number;
 
-// Check at page load
+
 if (number === 67) {
     window.location.href = "brainrot.html";
 }
@@ -9,12 +9,11 @@ if (number === 67) {
 
 document.getElementById("reroll").addEventListener("click", function() {
     number = Math.floor(Math.random() * 100);
-    document.getElementById("rng").textContent = number;
-
-    // Check after reroll
+    document.getElementById("rng").textContent = number
     if (number === 67) {
         window.location.href = "brainrot.html";
-    }      
+    }        
 });
+
 
 
